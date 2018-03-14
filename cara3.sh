0781c5f3f30dc8ba8ff471caf866ea8027b3a409
Famousxploit <37292122+Famousxploit@users.noreply.github.com>
2018-03-14 23:37:04 +0700

    Create cara3.sh

---
+++ cara3.sh
@@ -0,0 +1,53 @@
+#!/system/xbin/bash
+clear
+toilet -f slant --gay "Hack"
+sleep 1
+echo "\033[31;1m Cara memakai tools hack"
+sleep 1
+echo "\033[33;1m Author: Walker#04"
+sleep 1
+echo "\033[32;1m Contact: +16285006979"
+sleep 1
+echo "\033[34;1m Famous Xploit Team"
+sleep 1
+echo "\033[36;1m Pilih Yang Tersedia :"
+echo "1. FB-Bruteforce"
+echo "0. Kembali"
+echo "\033[33;1m Pilih Angka:"
+read mrrm
+
+if
+[ $mrrm = 1 ] || [ $mrrm = 1 ]
+then
+clear
+toilet -f slant --gay "FB-Bruteforce"
+echo "\033[31;1mNama tools: FB-Bruteforce"
+sleep 1
+echo "\033[33;1m1.Buka browser masuk ke fb kalian cari mana yang mau di hack copy link nya"
+sleep 1
+echo "\033[32;1mContoh: https://mobile.facebook.comfbid=1893563707637904&id=100009528791467&set=a"
+sleep 1
+echo "\033[36;1mLalu copy id dari facebook korban"
+sleep 1
+echo "\033[34;1mPastekan pada termux"
+sleep 1
+echo "\033[36;1mLalu copy id dari facebook korban"
+sleep 1
+echo "\033[36;1mKalau udah masuin password script nya dengan perintah"
+sleep 1
+echo "\033[36;1mJika di sd card = /sdcard/password.txt"
+sleep 1
+echo "\033[36;1mJika di Internal = /storage/emulated/0/password.txt"
+echo "\033[30;1m tunggu 8 detik"
+sleep 30
+sh cara3.sh
+fi
+
+if
+[ $mrrm = 0 ] || [ $mrrm = 0 ]
+then
+clear
+echo "\033[31;1m"
+toilet "Kembali"
+sh Hack.sh
+fi
