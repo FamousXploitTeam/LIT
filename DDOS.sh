e8b48296a3b84e4f60faa787f57939309607c369
Famousxploit <37292122+Famousxploit@users.noreply.github.com>
2018-03-14 22:59:35 +0700

    Create DDOS.sh

---
+++ DDOS.sh
@@ -0,0 +1,44 @@
+#!/system/xbin/bash
+clear
+toilet -f slant --gay "Spam"
+sleep 1
+echo "\033[31;1m Macam-macam tool untuk spam"
+sleep 1
+echo "\033[33;1m Author: Walker#04"
+sleep 1
+echo "\033[32;1m Contact: +16285006979"
+sleep 1
+echo "\033[34;1m Famous Xploit Team"
+sleep 1
+echo "\033[36;1m Spam Yang Tersedia :"
+echo "1. Torshammer"
+echo "\033[30;1m99. Cara Memakai Tools Ini"
+echo "\033[33;1m Pilih Angka:"
+read mrrm
+
+if
+[ $mrrm = 1 ] || [ $mrrm = 1 ]
+then
+clear
+echo "\033[31;1m"
+toilet "Torshammer"
+python2 torshammer.py
+fi
+
+if
+[ $mrrm = 99 ] || [ $mrrm = 99 ]
+then
+clear
+toilet -f mono12 -F gay "Info"
+echo "\033[33;1m"
+sh cara2.sh
+fi
+
+if
+[ $mrrm = 0 ] || [ $mrrm = 0 ]
+then
+echo "\033[31;1m Keluar"
+sleep 1
+echo "\033[32;1m See You Later! :)"
+sleep 1
+fi
