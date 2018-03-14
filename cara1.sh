75661ddd2344d44dcfc03430e33385923f98f7e0
Famousxploit <37292122+Famousxploit@users.noreply.github.com>
2018-03-15 00:02:43 +0700

    Create cara1.sh

---
+++ cara1.sh
@@ -0,0 +1,42 @@
+#!/system/xbin/bash
+clear
+toilet -f slant --gay "SPAM"
+sleep 1
+echo "\033[31;1m Cara memakai tools spam"
+sleep 1
+echo "\033[33;1m Author: Walker#04"
+sleep 1
+echo "\033[32;1m Contact: +16285006979"
+sleep 1
+echo "\033[34;1m Famous Xploit Team"
+sleep 1
+echo "\033[36;1m Pilih Yang Tersedia :"
+echo "1. All Spam"
+echo "0. Kembali"
+echo "\033[33;1m Pilih Angka:"
+read mrrm
+
+if
+[ $mrrm = 1 ] || [ $mrrm = 1 ]
+then
+clear
+toilet -f slant --gay "Spam"
+Next Pilih sesuai kebutuhan kalian
+sleep 1
+echo "\033[33;1mlalu ikuti step by step"
+sleep 1
+echo "\033[32;1mSudah ada kok pada menunya"
+sleep 1
+echo "\033[30;1m tunggu 5 detik"
+sleep 5
+sh cara1.sh
+fi
+
+if
+[ $mrrm = 0 ] || [ $mrrm = 0 ]
+then
+clear
+echo "\033[31;1m"
+toilet "Kembali"
+sh SPAM.sh
+fi
