e62ae355eee2a88247d754809a855e173250a45a
Famousxploit <37292122+Famousxploit@users.noreply.github.com>
2018-03-14 23:59:32 +0700

    Create cara2.sh

---
+++ cara2.sh
@@ -0,0 +1,48 @@
+#!/system/xbin/bash
+clear
+toilet -f slant --gay "DDOS"
+sleep 1
+echo "\033[31;1m Cara memakai tools ddos"
+sleep 1
+echo "\033[33;1m Author: Walker#04"
+sleep 1
+echo "\033[32;1m Contact: +16285006979"
+sleep 1
+echo "\033[34;1m Famous Xploit Team"
+sleep 1
+echo "\033[36;1m Pilih Yang Tersedia :"
+echo "1. Torshammer"
+echo "0. Kembali"
+echo "\033[33;1m Pilih Angka:"
+read mrrm
+
+if
+[ $mrrm = 1 ] || [ $mrrm = 1 ]
+then
+clear
+toilet -f slant --gay "Torshammer"
+echo "\033[31;1mNama tools: Torshammer"
+sleep 1
+echo "\033[33;1m1.Buka browser untuk mencari target"
+sleep 1
+echo "\033[32;1mLalu copy link target"
+sleep 1
+echo "\033[36;1mContoh : www.google.com"
+sleep 1
+echo "\033[34;1mlalu tulis perintah sebagai berikut"
+sleep 1
+echo "\033[36;1m$python2 torshammer.py -t <taget>"
+sleep 1
+echo "\033[30;1m tunggu 10 detik"
+sleep 10
+sh cara2.sh
+fi
+
+if
+[ $mrrm = 0 ] || [ $mrrm = 0 ]
+then
+clear
+echo "\033[31;1m"
+toilet "Kembali"
+sh DDOS.sh
+fi
