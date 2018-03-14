19d2702e8c95b09fdb057760dfa8d7fa987e5a54
Famousxploit <37292122+Famousxploit@users.noreply.github.com>
2018-03-14 23:24:59 +0700

    Create new file

---
+++ Hack.sh
@@ -0,0 +1,45 @@
+#!/system/xbin/bash
+clear
+toilet -f slant --gay "Spam"
+sleep 1
+echo "\033[31;1m Macam-macam tool untuk hack"
+sleep 1
+echo "\033[33;1m Author: Walker#04"
+sleep 1
+echo "\033[32;1m Contact: +16285006979"
+sleep 1
+echo "\033[34;1m Famous Xploit Team"
+sleep 1
+echo "\033[36;1m Hack Yang Tersedia :"
+echo "1. FB-Bruteforce"
+echo "0. Kembali"
+echo "\033[30;1m99. Cara Memakai Tools Ini"
+echo "\033[33;1m Pilih Angka:"
+read mrrm
+
+if
+[ $mrrm = 1 ] || [ $mrrm = 1 ]
+then
+clear
+echo "\033[31;1m"
+toilet "fb-brute"
+python2 fbbrute.py
+fi
+
+if
+[ $mrrm = 99 ] || [ $mrrm = 99 ]
+then
+clear
+toilet -f mono12 -F gay "Info"
+echo "\033[33;1m"
+sh cara3.sh
+fi
+
+if
+[ $mrrm = 0 ] || [ $mrrm = 0 ]
+then
+clear
+echo "\033[31;1m"
+toilet "Kembali"
+sh Tools.sh
+fi
