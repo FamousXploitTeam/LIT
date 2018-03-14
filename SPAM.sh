c5ee4a12dec35920fe7a5def2c60ff0e724fd6f3
Famousxploit <37292122+Famousxploit@users.noreply.github.com>
2018-03-14 22:41:23 +0700

    Create SPAM.sh

---
+++ SPAM.sh
@@ -0,0 +1,85 @@
+#!/system/xbin/bash
+clear
+toilet -f slant --gay "LiteSpam"
+sleep 1
+echo "\033[31;1m Macam-macam tool untuk spam"
+sleep 1
+echo "\033[33;1m Author: Walker#04"
+sleep 1
+echo "\033[32;1m Contact: +16285006979"
+sleep 1
+echo "\033[34;1m Famous Xploit Team"
+sleep 1
+echo "\033[36;1m Spam Yang Tersedia :"
+echo "1. Telkomsel"
+echo "2. Tokopedia"
+echo "3. Matahari Mall"
+echo "4. PHD"
+echo "5. Jd.Id"
+echo "0. Keluar"
+echo "\033[30;1m99. Cara Memakai Tools Ini"
+echo "\033[33;1m Pilih Angka:"
+read mrrm
+
+if
+[ $mrrm = 1 ] || [ $mrrm = 1 ]
+then
+clear
+echo "\033[31;1m"
+toilet "T-Sel"
+php telkomsel.php
+fi
+
+if
+[ $mrrm = 2 ] || [ $mrrm = 2 ]
+then
+clear
+echo "\033[31;1m"
+toilet "tokopedia"
+php tokopedia.php
+fi
+
+if
+[ $mrrm = 3 ] || [ $mrrm = 3 ]
+then
+clear
+echo "\033[31;1m"
+toilet "Matahari Mall"
+php Matahari.php
+fi
+
+if
+[ $mrrm = 4 ] || [ $mrrm = 4 ]
+then
+clear
+toilet -f mono12 -F gay "PHD"
+echo "\033[30;1m"
+php PHD.php
+fi
+
+if
+[ $mrrm = 5 ] || [ $mrrm = 5 ]
+then
+clear
+toilet -f mono12 -F gay "Jd.Id"
+echo "\033[33;1m"
+php JDID.php
+fi
+
+if
+[ $mrrm = 99 ] || [ $mrrm = 99 ]
+then
+clear
+toilet -f mono12 -F gay "Info"
+echo "\033[33;1m"
+sh Info1.sh
+fi
+
+if
+[ $mrrm = 0 ] || [ $mrrm = 0 ]
+then
+echo "\033[31;1m Keluar"
+sleep 1
+echo "\033[32;1m See You Later! :)"
+sleep 1
+fi
