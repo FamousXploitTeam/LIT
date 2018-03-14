559dfd9af396d4c86ea538f65b7031e0beda2edd
Famousxploit <37292122+Famousxploit@users.noreply.github.com>
2018-03-14 23:12:54 +0700

    Create info.sh

---
+++ info.sh
@@ -0,0 +1,24 @@
+#!/system/xbin/bash
+clear
+toilet -f slant --gay "Tools"
+echo "\033[31;1mCreated By: Walker#04"
+sleep 1
+echo "\033[32;1mVersi: v1.2"
+sleep 1
+echo "\033[36;1mTeam: Famous Xploit"
+sleep 1
+echo "\033[31;1m Spesial Thanks To: "
+echo "\033[36;1m"
+echo "Arex007"
+echo "Handika Pratama"
+echo "Bang Djon"
+echo "All Member Famous Xploit Team"
+
+if
+[ $mrrm = 0 ] || [ $mrrm = 0 ]
+then
+clear
+echo "\033[31;1m"
+toilet "Tools"
+sh Tools.sh
+fi
